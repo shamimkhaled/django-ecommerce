49ad2ca924db8bd9ca814d2050178c5e9b1ae282
shamimkhaled <i.amkhaled89@gmail.com>
2023-02-16 22:46:34 +0600

    initial commit

---
+++ greatkart/accounts/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+
+# for super admin user model
+class MyAccountManager(BaseUserManager, ):
+    def create_user(self, first_name, last_name, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email addres')
+        if not username:
+            raise ValueError('User must have an unique username')
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            **extra_fields
+            
+        )
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+            is_admin = True,
+            is_active = True,
+            is_staff = True,
+            is_superadmin = True,
+        )
+        
+        #user.save(using = self._db)
+        return user
+       
+       
+        
+
+
+
+# for custom user model
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    phone = models.CharField(max_length=50)
+
+    #required
+    date_joined = models.DateField(auto_now_add=True)
+    last_login = models.DateField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    objects = MyAccountManager()
+
+    # change username field to replace email field and other required fiels
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+   
+
+    def __str__(self):
+        return self.email
+
+    # if the user is admin, he has to all permissions
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True 
+
+ 
+
---
+++ greatkart/store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+#from django.conf import settings
+#from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    # Products by Category Slug URL 
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+    # Product Detail URL
+    path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name='product_detail'),
+] 
